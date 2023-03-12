c8b81ac252c8e3d618501ee5d2014255127deab0
Zawalisz <marcin.zawalisz@gmail.com>
2023-03-12 22:32:48 +0100

    adding is_prime and test_is_prime

---
+++ is_prime.py
@@ -0,0 +1,7 @@
+def is_prime(number):
+	if(abs(number) <= 1):
+		return False
+	for i in range(2, number):
+		if(number % i == 0):
+			return False
+	return True
---
+++ test_is_prime.py
@@ -0,0 +1,6 @@
+from is_prime import is_prime
+
+@pytest.mark.parametrize("number, result", [(2,True), (3,True), (4,False), (5,True), (6,False), (7,True), (8,False), (9, False)])
+
+def test_is_prime(number, result):
+    assert is_prime(number) == result
